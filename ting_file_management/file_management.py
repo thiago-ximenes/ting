266abd441b3c207166315027eaee3b0c27a06792
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-16 08:38:44 -0300

    pass: pass requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+import os.path
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    with open(path_file, "r") as file:
+        content = file.read()
+        contentList = content.splitlines()
+
+    return contentList
