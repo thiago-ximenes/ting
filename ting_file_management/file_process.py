e50e076bea7a8b9f2e75f426394c9b5a5b046f32
Thiago Ximenes <thiago.ximenes@paylivre.com>
2022-09-16 15:10:53 -0300

    wip: salva pra continuar depois

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_lines = txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines,
+    }
+    if len(instance) == 0:
+        return instance.enqueue(result)
+    else:
+        for file in instance.data:
+            if file["nome_do_arquivo"] == path_file:
+                pass
+
+    return instance.enqueue(result)
 
 
 def remove(instance):
