ad7fa2787162948efb5c8ad5cf4198f303016480
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-17 23:44:31 -0300

    pass: pass requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -19,7 +19,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance.data) == 0:
+        return sys.stdout.write("Não há elementos\n")
+
+    file = instance.dequeue()
+    path_name = file["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {path_name} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
