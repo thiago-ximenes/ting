2e934cafec4cd324687127c623c07942fc4fb351
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-17 23:27:26 -0300

    pass: pass requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,21 +1,21 @@
-from file_management import txt_importer
+from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
+    for file in instance.data:
+        if file["nome_do_arquivo"] == path_file:
+            return None
+
     file_lines = txt_importer(path_file)
     result = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(file_lines),
         "linhas_do_arquivo": file_lines,
     }
-    if len(instance) == 0:
-        return instance.enqueue(result)
-    else:
-        for file in instance.data:
-            if file["nome_do_arquivo"] == path_file:
-                pass
 
-    return instance.enqueue(result)
+    instance.enqueue(result)
+    sys.stdout.write(str(result))
 
 
 def remove(instance):
