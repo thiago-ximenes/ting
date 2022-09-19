53d3b4ca77975d497950d76915cd8e3f55f7b103
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-19 08:58:39 -0300

    wip: salva progresso pra continuar depois

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_with_that_word = []
+
+    for file in instance.data:
+        for line in file["linhas_do_arquivo"]:
+            if word in line:
+                files_with_that_word.append(file)
+                break
+
+    result = []
+
+    for file in files_with_that_word:
+        result.append({
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            
+        })
 
 
 def search_by_word(word, instance):
